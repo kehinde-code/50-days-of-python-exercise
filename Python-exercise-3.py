464fe86f1d1b03f53e94b0ff93da67b1d46e0756
kehinde omosor <kehindeomosor@gmail.com>
2024-10-28 22:50:26 -0500

    Track untracked files

---
+++ Python-exercise-3.py
@@ -0,0 +1,10 @@
+#def odd_number(y =7):
+#def even_number(x = 6):
+   # return 
+def is_even(number):
+    return number % 2 == 0
+
+def is_odd(number): 
+    return number % 2 != 0
+print(is_even(3))
+
