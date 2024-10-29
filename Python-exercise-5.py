8e4109d90888c669c0cd16e5a368cde9de59cb17
kehinde omosor <kehindeomosor@gmail.com>
2024-10-28 21:54:57 -0500

    fifth commit

---
+++ Python-exercise-5.py
@@ -0,0 +1,19 @@
+def fizzBuzz(upTo):
+
+ #for y in range(1 upTo):
+ 
+ if y % 3 == 0 and y % 5 == 0:
+    return "FizzBuzz" 
+ elif y % 3 == 0:
+    return "Fizz"
+ elif y % 5 == 0: 
+   return "BUzz"
+ else:
+   return str(y)
+ 
+ for i in range(1, 5):
+
+ 
+
+  print(fizzBuzz)
+
